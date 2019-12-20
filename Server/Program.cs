c33d91a2935b5ba0c821de97d6982f14d7ac9c08
calum-bird <calumbird3@gmail.com>
2019-12-19 17:06:48 -0800

    Setup communication between client and server

---
+++ Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using LiteNetLib;
+using LiteNetLib.Utils;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing image sending server!");
+
+            //Initialize server on port 3001
+            EventBasedNetListener listener = new EventBasedNetListener();
+            NetManager server = new NetManager(listener);
+            server.Start(3001 /* port */);
+
+            listener.ConnectionRequestEvent += request =>
+            {
+                if(server.PeersCount < 10 /* max connections */)
+                    request.AcceptIfKey("SomeConnectionKey");
+                else
+                    request.Reject();
+            };
+
+            listener.PeerConnectedEvent += peer =>
+            {
+                Console.WriteLine("We got connection: {0}", peer.EndPoint); // Show peer ip
+                NetDataWriter writer = new NetDataWriter();                 // Create writer class
+                writer.Put("Hello client!");                                // Put some string
+                peer.Send(writer, DeliveryMethod.ReliableOrdered);             // Send with reliability
+            };
+
+            while (!Console.KeyAvailable)
+            {
+                server.PollEvents();
+                Thread.Sleep(15);
+            }
+            server.Stop();
+        }
+    }
+}
