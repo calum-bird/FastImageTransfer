c53a48c19a2266408c07325f268b205ea9aeb23e
calum-bird <calumbird3@gmail.com>
2019-12-19 16:46:19 -0800

    Initialized dotnet console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastImageTransfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
